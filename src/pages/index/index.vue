e2cb19403cbe9c6fa24326ff6beba74134822e19
aierong <aierong@126.com>
2019-03-22 18:24:27 +0800

    0

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
     {{ msg }}
+    <br>
+    <button open-type="getUserInfo"
+            lang="zh_CN"
+            @getuserinfo="onGotUserInfo">获取用户信息</button>
+
   </div>
+
+
+
+
+
+
+
 </template>
 
 <script>
@@ -9,24 +21,44 @@
   export default {
     data () {
       return {
-        msg : "ss"
+        msg : "ss" ,
+        userinfo : {}
 
       };
     } ,
 
-    components : {} ,
-
     methods : {
 
-      // clickHandle ( ev ) {
-      //   console.log( "clickHandle:" , ev );
-      //   // throw {message: 'custom test'}
-      // }
+      onGotUserInfo ( data ) {
+        //console.log( data )
+
+        if ( data.mp.detail.rawData ) {
+          console.log( '同意了' )
+        }
+        else {
+          console.log( '拒绝' )
+        }
+      } ,
     } ,
 
     created () {
       // let app = getApp()
-    }
+    } ,
+    //生命周期(mounted)
+    mounted () {
+      //console.log( 'mounted' )
+
+      wx.getUserInfo( {
+        success : ( data ) => {
+          console.log( data )
+        } ,
+        fail : ( err ) => {
+          console.log( '获取失败' )
+          console.log( err )
+        }
+      } )
+
+    } ,
   };
 </script>
 
