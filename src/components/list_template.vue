72f38cc586794b06aa3ef9ebf60111bee719c6bc
aierong <aierong@126.com>
2019-03-25 16:22:45 +0800

    ==============

--- src/components/list_template.vue
+++ src/components/list_template.vue
@@ -6,8 +6,26 @@
 <!-- html代码片段 -->
 <template>
 
-  <div>
-    list_templatelist_template
+  <div @tap="toDetail"
+       class="tmpContainer">
+    <div class="avatar_data">
+      <img :src="item.avatar"
+           alt="">
+      <span>{{item.date}}</span>
+    </div>
+    <p class="company">{{item.title}}</p>
+    <img class="detail_img"
+         :src="item.detail_img"
+         alt=""/>
+    <p class="content">{{item.detail_content}}</p>
+    <div class="view_star_container">
+      <img src="/static/images/icon/star.png"
+           alt=""/>
+      <span>{{item.love_count}}</span>
+      <img src="/static/images/icon/view.png"
+           alt=""/>
+      <span>{{item.attention_count}}</span>
+    </div>
   </div>
 
 </template>
