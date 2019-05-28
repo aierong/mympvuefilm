ecd092f23be0d4b8e1a3fbbc2ee4972e38b84f68
aierong <aierong@126.com>
2019-05-28 09:14:50 +0800

    company

--- src/store/store.js
+++ src/store/store.js
@@ -6,12 +6,18 @@ import actions from './actions'
 import mutations from './mutations'
 import getters from './getters'
 
+import createLogger from 'vuex/dist/logger'
+
 // 声明使用vuex
 Vue.use( Vuex )
+
+const isdebug = process.env.NODE_ENV !== 'production'
+// console.log( 'debug' , debug )
 
 export default new Vuex.Store( {
   state ,
   actions ,
   getters ,
-  mutations
+  mutations ,
+  plugins : isdebug ? [ createLogger() ] : [] // 在开发环境下，配置日志 。
 } )
